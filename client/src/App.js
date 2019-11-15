1e270b93fdcbf0489ab0d411a6f3905cb59d3a14
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-15 10:46:01 -0800

    PrivateRoute Comp // Bubble Authenticator

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
+/* ------------------------------------------------- */
+
 import React, { useState } from "react";
+// removed { useState}
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+/* ------------------------------------------------- */
 
 import Login from "./components/Login";
 import "./styles.scss";
+
+/* ------------------------------------------------- */
+// brought in files but they aren't lighting up?
+// export default was working. confused
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
+// nevermind, needed to make a PrivateRoute component
+// to display BubblePage when autheticated. 
+
+/* ------------------------------------------------- */
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/bubbles" component={BubblePage} />
       </div>
     </Router>
   );
 }
 
+/* ------------------------------------------------- */
+
 export default App;
+
+/* ------------------------------------------------- */
