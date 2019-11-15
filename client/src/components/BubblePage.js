c5b9db419717014f03e1943181a70620a89fd718
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-15 10:39:53 -0800

    Step 2 Bubble Page Render

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,38 @@
+/* ------------------------------------------------- */
+
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// removing the line ' import axios from "axios" ' ;
+
+/* ------------------------------------------------- */
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+
+/* ------------------------------------------------- */
+// adding the import ; 
+import { axiosWithAuth } from "../utilities/axiosWithAuth";
+
+/* ------------------------------------------------- */
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  /* ------------------------------------------------- */
+
+  useEffect(() => {
+    axiosWithAuth()
+    .get("http://localhost:5000/api/colors")
+    .then(response => {
+      console.log("Get Response", response.data)
+      setColorList(response.data)
+      console.log("colorList", colorList)
+    })
+    .catch(error => {
+      console.log("Get Error", error)
+    })
+  }, [])
 
   return (
     <>
@@ -17,4 +42,8 @@ const BubblePage = () => {
   );
 };
 
+/* ------------------------------------------------- */
+
 export default BubblePage;
+
+/* ------------------------------------------------- */
