98fb431ea02d345f6c678b39d52b053a68485dc6
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-15 10:47:37 -0800

    axiosAuth

--- client/src/utilities/axiosWithAuth.js
+++ client/src/utilities/axiosWithAuth.js
@@ -6,7 +6,8 @@ import axios from "axios";
 
 export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
-   
+    //return a new instance of axios
+
     return axios.create({
         baseURL: 'http://localhost:5000',
         headers: {
