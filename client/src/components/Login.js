8e9e586cbbe5edd8cf9ea46074a40f16cb47e3f6
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-15 10:52:55 -0800

    First half of Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,51 @@
+/* ------------------------------------------------- */
+
 import React from "react";
+
+/* ------------------------------------------------- */
+// adding import
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+/* ------------------------------------------------- */
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({ username: '', password: '' })
+
+/* ------------------------------------------------- */
+
+  const handleChange = event => {
+    setCredentials(
+      {
+        ...credentials,
+        [event.target.name]: event.target.value
+      }
+    )
+  }
+
+/* ------------------------------------------------- */
+
+const onSubmit = event => {
+  event.preventDefault();
+  axiosWithAuth()
+  .post('/api/login', credentials)
+  .then(response => {
+    console.log("Post Response", response)
+    localStorage.setItem('token', response.data.payload)
+    props.history.push("/bubbles")
+  })
+
+/* ------------------------------------------------- */
+
+.catch(error => {
+  console.log(error)
+  setCredentials({username: "", password: ""})
+})
+}
+
+/* ------------------------------------------------- */
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
@@ -11,4 +54,8 @@ const Login = () => {
   );
 };
 
+/* ------------------------------------------------- */
+
 export default Login;
+
+/* ------------------------------------------------- */
