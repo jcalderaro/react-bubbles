08cf1b1dd49b9297a1f554eb346d5fa1ec8e05ea
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-15 10:55:30 -0800

    Login Form Rendered

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
   // when you have handled the token, navigate to the BubblePage route
   const [credentials, setCredentials] = useState({ username: '', password: '' })
 
-/* ------------------------------------------------- */
+  /* ------------------------------------------------- */
 
   const handleChange = event => {
     setCredentials(
@@ -24,34 +24,61 @@ const Login = () => {
     )
   }
 
-/* ------------------------------------------------- */
+  /* ------------------------------------------------- */
 
-const onSubmit = event => {
-  event.preventDefault();
-  axiosWithAuth()
-  .post('/api/login', credentials)
-  .then(response => {
-    console.log("Post Response", response)
-    localStorage.setItem('token', response.data.payload)
-    props.history.push("/bubbles")
-  })
+  const onSubmit = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post('/api/login', credentials)
+      .then(response => {
+        console.log("Post Response", response)
+        localStorage.setItem('token', response.data.payload)
+        props.history.push("/bubbles")
+      })
 
-/* ------------------------------------------------- */
+      /* ------------------------------------------------- */
 
-.catch(error => {
-  console.log(error)
-  setCredentials({username: "", password: ""})
-})
-}
+      .catch(error => {
+        console.log(error)
+        setCredentials({ username: "", password: "" })
+      })
+  }
 
-/* ------------------------------------------------- */
-// need to return the form and display the input strings
-// also need to apply a component to invoke handleChange and onSubmit
+  /* ------------------------------------------------- */
+  // need to return the form and display the input strings
+  // also need to apply a component to invoke handleChange and onSubmit
 
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <div>
+
+        <form onSubmit={onSubmit}>
+          <div>
+            <input
+              type="text"
+              name="username"
+              placeholder="User Name"
+              value={credentials.username}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={credentials.password}
+              onChange={handleChange}
+            />
+          </div>
+
+          <button>Login</button>
+
+        </form>
+      </div>
     </>
   );
 };
