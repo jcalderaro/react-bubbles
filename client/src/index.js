1ae8b05e3599289c3295954d38bf3b89b2cca35f
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-15 10:43:01 -0800

    Removed useState from Index

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
+/* ------------------------------------------------- */
+
 import React, { useState } from "react";
+
+/* ------------------------------------------------- */
+
+// removed { useState } from line 3 due to faded color?
+
 import ReactDOM from "react-dom";
 import App from "./App";
 
+/* ------------------------------------------------- */
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+/* ------------------------------------------------- */
